819ce83e148191df87d8f311414581f36ea5e6df
MCOUCK <michael.couck@vdba.be>
2019-01-18 12:18:40 +0100

    QUER-5: Micro services componenten gedeployed en operationeel.

--- build-deploy-all.sh
+++ build-deploy-all.sh
@@ -6,22 +6,19 @@ USER=mcouck
 APP=${PWD##*/}
 for SERVER in "$@"
 do
-    echo Deploying to "$SERVER", killing app "$APP"
-    ssh-copy-id -i ~/.ssh/id_rsa.pub mcouck@$SERVER
-    sleep 1
+    # echo Deploying to "$SERVER", killing app "$APP"
+    # ssh-copy-id -i ~/.ssh/id_rsa.pub mcouck@$SERVER
+    # sleep 1
 
-    ssh $USER@$SERVER 'ps -ef | grep java'
     ssh $USER@$SERVER 'sudo pkill -9 -f java'
 
     # Deploy the certificate
-    ssh $USER@$SERVER 'sudo touch /etc/ssl/certs/vdab.jks; sudo chmod 777 /etc/ssl/certs/vdab.jks'
     scp C:/etc/ssl/certs/vdab.jks $USER@$SERVER:/etc/ssl/certs
     ssh $USER@$SERVER 'sudo chmod 777 /etc/ssl/certs/vdab.jks'
 
     # Deploy the application
-    ssh $USER@$SERVER 'sudo mkdir -p /opt/eureka-server; sudo chmod 777 -R /opt/eureka-server'
-    ssh $USER@$SERVER 'sudo chmod 777 -R /opt/eureka-server'
+    ssh $USER@$SERVER 'sudo mkdir -p /opt/eureka-server; sudo chmod 777 -R /opt/eureka-server/*; sudo rm /opt/eureka-server/nohup.out'
     scp target/$APP-1.0.0.jar $USER@$SERVER:/opt/$APP
-    ssh $USER@$SERVER 'sudo chmod 777 /opt/eureka-server/*; ls -lh /opt/eureka-server'
+    ssh $USER@$SERVER 'sudo chmod 777 /opt/eureka-server/*; '
     ssh $USER@$SERVER 'cd /opt/eureka-server; java -jar -Dspring.profiles.active=sand-box eureka-server-1.0.0.jar >> nohup.out &' &
 done
